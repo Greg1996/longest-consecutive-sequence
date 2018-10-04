82922ff1087d70c1e40959ea22f91a3cf2a590ad
greg <varankov@list.ru>
2018-10-04 14:14:03 +0300

    longest-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let count = 1;
+  let countMax = 1;
+  if(array.length === 0) return 0;
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  for(let i=0; i<array.length; i++){
+    if(array[i+1] == (array[i] + 1)){
+        count++;
+    }
+      else if (array[i + 1] - array[i] > 1){
+            countMax = Math.max(count, countMax);
+            count = 1;
+      }
+  }
+  return countMax ;
 }
